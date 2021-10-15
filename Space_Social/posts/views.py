339372d5054d3b833a3f9258f6d40a9ef0ffe51b
August Schleich <61630956+A4Atom@users.noreply.github.com>
2021-10-15 11:10:41 -0700

    Setup: post- views.py

--- Space_Social/posts/views.py
+++ Space_Social/posts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 # Create your views here.
+from django.http import Http404
+from django.views import generic
+
+from braces.views import SelectRelatedMixin 
+
+from . import forms 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+
+class PostList(SelectRelatedMixin,generic.ListView):
+    pass
+    
