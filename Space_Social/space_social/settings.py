ea35395dfe824157898fcbe0705f360551c4eaf6
August Schleich <61630956+A4Atom@users.noreply.github.com>
2021-12-27 12:12:32 -0800

    fixed image padding index.html

--- Space_Social/space_social/settings.py
+++ Space_Social/space_social/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1p3)jaeipz^n%j92(lq-%cib$t*uy%(_1lsj!tkjmd(m#qtb%y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.3','127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.3','127.0.0.1','192.168.1.2']
 
 
 # Application definition
